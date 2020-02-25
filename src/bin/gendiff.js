9bd1635c5e1e90543cbb20990af9a5a15022f5e7
gorushkin <gorushkin@gmail.com>
2020-02-25 22:12:59 +0300

    step1

---
+++ src/bin/gendiff.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import program from 'commander';
+import { version } from '../../package.json';
+
+program
+  .version(version)
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv);
