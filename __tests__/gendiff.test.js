d63693a5f24ac6693b87bcaf4dcf4cf7c5c850f9
gorushkin <gorushkin@gmail.com>
2020-03-02 19:36:44 +0000

    step4.5

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,20 +1,34 @@
 import fs from 'fs';
 import gendiff from '../src/index';
 
-test('чтение и сравнение файлов', () => {
-  const pathFirst = `${__dirname}/fixtures/before.json`;
-  const pathSecond = `${__dirname}/fixtures/after.json`;
-  const pathResult = `${__dirname}/fixtures/result1`;
-  const secondResult = fs.readFileSync(pathResult, 'utf-8');
-  const result = gendiff(pathFirst, pathSecond);
-  expect(secondResult).toEqual(result);
-});
+const jsonPaths = {
+  pathFirst: `${__dirname}/fixtures/before.json`,
+  pathSecond: `${__dirname}/fixtures/after.json`,
+};
 
-test('чтение и сравнение файлов в обратном порядке', () => {
-  const pathFirst = `${__dirname}/fixtures/after.json`;
-  const pathSecond = `${__dirname}/fixtures/before.json`;
-  const pathResult = `${__dirname}/fixtures/result2`;
-  const secondResult = fs.readFileSync(pathResult, 'utf-8');
-  const result = gendiff(pathFirst, pathSecond);
-  expect(secondResult).toEqual(result);
-});
+const ymlPaths = {
+  pathFirst: `${__dirname}/fixtures/before.yml`,
+  pathSecond: `${__dirname}/fixtures/after.yml`,
+};
+
+const resulPaths = {
+  result1: `${__dirname}/fixtures/result1`,
+  result2: `${__dirname}/fixtures/result2`,
+  
+}
+
+describe('jsonPaths', () => {
+  test('чтение и сравнение файлов JSON', () => {
+    const pathResult = `${__dirname}/fixtures/result1`;
+    const secondResult = fs.readFileSync(pathResult, 'utf-8');
+    const result = gendiff(jsonPaths.pathFirst, jsonPaths.pathSecond);
+    expect(secondResult).toEqual(result);
+  });
+  
+  test('чтение и сравнение файлов в обратном порядке JSON', () => {
+    const pathResult = `${__dirname}/fixtures/result2`;
+    const secondResult = fs.readFileSync(pathResult, 'utf-8');
+    const result = gendiff(jsonPaths.pathSecond, jsonPaths.pathFirst);
+    expect(secondResult).toEqual(result);
+  });
+})
