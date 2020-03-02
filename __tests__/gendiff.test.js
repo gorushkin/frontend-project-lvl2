0ddcae3c42daea2e82d5d75ed6e444253858ab7e
gorushkin <gorushkin@gmail.com>
2020-03-02 17:12:50 +0000

    добавил второй тест

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,7 +4,16 @@ import gendiff from '../src/index';
 test('чтение и сравнение файлов', () => {
   const pathFirst = `${__dirname}/fixtures/before.json`;
   const pathSecond = `${__dirname}/fixtures/after.json`;
-  const pathResult = `${__dirname}/fixtures/result`;
+  const pathResult = `${__dirname}/fixtures/result1`;
+  const secondResult = fs.readFileSync(pathResult, 'utf-8');
+  const result = gendiff(pathFirst, pathSecond);
+  expect(secondResult).toEqual(result);
+});
+
+test('чтение и сравнение файлов в обратном порядке', () => {
+  const pathFirst = `${__dirname}/fixtures/after.json`;
+  const pathSecond = `${__dirname}/fixtures/before.json`;
+  const pathResult = `${__dirname}/fixtures/result2`;
   const secondResult = fs.readFileSync(pathResult, 'utf-8');
   const result = gendiff(pathFirst, pathSecond);
   expect(secondResult).toEqual(result);
