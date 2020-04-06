a7af9201a5f114dbb59a3b47f51f844d48683bca
gorushkin <gorushkin@gmail.com>
2020-04-06 09:13:42 +0000

    переделал тесты

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,24 +3,6 @@ import gendiff from '../src/index';
 
 const fixturesPath = `${__dirname}/../__fixtures__/`;
 
-const getPath = (type, order) => `${fixturesPath}${order}.${type}`;
-
-const correctFilesPaths = {
-  result1: `${fixturesPath}fullFomatDirectOrder.txt`,
-  result2: `${fixturesPath}fullFomatReversOrder.txt`,
-  result3: `${fixturesPath}plainFomatDirectOrder.txt`,
-  result4: `${fixturesPath}plainFomatReversOrder.txt`,
-  result5: `${fixturesPath}jsonFormatDirectOrder.txt`,
-};
-
-const correctFiles = {
-  file1: fs.readFileSync(correctFilesPaths.result1, 'utf-8'),
-  file2: fs.readFileSync(correctFilesPaths.result2, 'utf-8'),
-  file3: fs.readFileSync(correctFilesPaths.result3, 'utf-8'),
-  file4: fs.readFileSync(correctFilesPaths.result4, 'utf-8'),
-  file5: fs.readFileSync(correctFilesPaths.result5, 'utf-8'),
-};
-
 const inputFileTypes = [
   ['json'],
   ['yml'],
@@ -28,18 +10,38 @@ const inputFileTypes = [
 ];
 
 const tests = [
-  ['before', 'after', 'full', correctFiles.file1],
-  ['after', 'before', 'full', correctFiles.file2],
-  ['before', 'after', 'plain', correctFiles.file3],
-  ['after', 'before', 'plain', correctFiles.file4],
-  ['before', 'after', 'json', correctFiles.file5],
+  ['directOrder', 'full'],
+  ['reversOrder', 'full'],
+  ['directOrder', 'plain'],
+  ['reversOrder', 'plain'],
+  ['directOrder', 'json'],
 ];
+
+const getOutputFile = (order, format) => {
+  const filePath = `${fixturesPath}${format}Format-${order}.txt`;
+  return fs.readFileSync(filePath, 'utf-8');
+}
+
+const getInputFilesPath = (order, type) => {
+  switch (order) {
+    case 'directOrder':
+      return [`${fixturesPath}before.${type}`, `${fixturesPath}after.${type}`];
+    case 'reversOrder':
+      return [`${fixturesPath}after.${type}`, `${fixturesPath}before.${type}`];
+    default:
+      console.log('wrong order');
+      break;
+  }
+  return [file1, file2];
+}
 
 describe.each(inputFileTypes)('type %s', (type) => {
   test.each(tests)(
-    `%s.${type} %s.${type} --format %s`,
-    (firstFile, secondFile, format, expectedResult) => {
-      const result = gendiff(getPath(type, firstFile), getPath(type, secondFile), format);
+    `test direction %s --format %s`,
+    (order, format) => {
+      const [file1, file2] = getInputFilesPath(order, type);
+      const expectedResult = getOutputFile(order, format);
+      const result = gendiff(file1, file2, format);
       expect(result).toEqual(expectedResult);
     },
   );
