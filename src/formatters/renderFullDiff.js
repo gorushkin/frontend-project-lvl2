993fbcc7b4cab67219df67027294dd623cbbf7c2
gorushkin <gorushkin@gmail.com>
2020-04-22 11:27:43 +0300

    1

--- src/formatters/renderFullDiff.js
+++ src/formatters/renderFullDiff.js
@@ -2,17 +2,17 @@ import _ from 'lodash';
 
 const stringify = (obj, depth) => {
   if (typeof obj !== 'object') {
-    return obj;
+    return `${obj}\n`;
   }
   const spaces = '  '.repeat(depth + 1);
   const notes = Object.keys(obj).map((key) => `${spaces}    ${key}: ${obj[key]}`);
-  const result = [`{\n${notes.join('\n')}\n${spaces}}`];
+  const result = [`{\n${notes.join('\n')}\n${spaces}}\n`];
   return result;
 };
 
 const notesGenerators = {
   unchanged: (spaces, note, depth) => `${spaces}  ${note.key}: ${stringify(note.before, depth)}`,
-  changed: (spaces, note, depth) => `${spaces}- ${note.key}: ${stringify(note.before, depth)}\n${spaces}+ ${note.key}: ${stringify(note.after, depth)}`,
+  changed: (spaces, note, depth) => `${spaces}- ${note.key}: ${stringify(note.before, depth)}${spaces}+ ${note.key}: ${stringify(note.after, depth)}`,
   added: (spaces, note, depth) => `${spaces}+ ${note.key}: ${stringify(note.after, depth)}`,
   removed: (spaces, note, depth) => `${spaces}- ${note.key}: ${stringify(note.before, depth)}`,
 };
@@ -23,7 +23,7 @@ const renderFullDiff = (array) => {
     const result = diff.map((note) => {
       if (_.has(note, 'children')) {
         const children = iter(note.children, depth + 2);
-        return [`${spaces}  ${note.key}: {`, ...children, `  ${spaces}}`];
+        return [`${spaces}  ${note.key}: {\n`, ...children, `  ${spaces}}\n`];
       }
       return notesGenerators[note.status](spaces, note, depth);
     });
@@ -31,7 +31,7 @@ const renderFullDiff = (array) => {
   };
   const rawArrow = iter(array, 1);
   const flatedArr = _.flattenDeep(rawArrow);
-  const result = `{\n${flatedArr.join('\n')}\n}`;
+  const result = `{\n${flatedArr.join('')}}`;
   return result;
 };
 
