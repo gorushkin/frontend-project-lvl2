e3d54484c98b7f76f5794896a42482f1b35c1b45
gorushkin <gorushkin@gmail.com>
2020-04-24 10:42:31 +0300

    ошибки линтера

--- src/generateDiff.js
+++ src/generateDiff.js
@@ -25,48 +25,46 @@ const sortData = (data) => {
   return result;
 };
 
-const getCurrentDiff = (key, obj1, obj2) => {
-  if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
-    const children = generateDiff(obj1[key], obj2[key]);
-    const type = 'parent';
-    const parentNode = { key, type, children };
-    return parentNode;
-  }
-  if ((_.has(obj1, key)) && (_.has(obj2, key))) {
-    const before = obj1[key];
-    const after = obj2[key];
-    const type = 'child';
-    const status = (before === after) ? 'unchanged' : 'changed';
-    const diff = { key, type, before, after, status };
-    return diff;
-  }
-  const type = 'child';
-  if ((_.has(obj1, key))) {
-    const before = obj1[key];
-    const status = 'removed';
-    const diff = { key, type, before, status };
-    return diff;
-  }
-  const after = obj2[key];
-  const status = 'added';
-  const diff = { key, type, after, status };
-  return diff;
-};
-
 const generateDiff = (obj1, obj2) => {
   const allKeys = _.union(_.keys(obj1), _.keys(obj2));
-  const unsortedData = allKeys.map((key) => {
-    const currentNode = getCurrentDiff(key, obj1, obj2);
-    return currentNode;
+  const result = allKeys.map((key) => {
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      const children = generateDiff(obj1[key], obj2[key]);
+      const type = 'parent';
+      const parentNode = { key, type, children };
+      return parentNode;
+    }
+    if ((_.has(obj1, key)) && (_.has(obj2, key))) {
+      const before = obj1[key];
+      const after = obj2[key];
+      const type = 'child';
+      const status = (before === after) ? 'unchanged' : 'changed';
+      const diff = {
+        key, type, before, after, status,
+      };
+      return diff;
+    }
+    const type = 'child';
+    if ((_.has(obj1, key))) {
+      const before = obj1[key];
+      const status = 'removed';
+      const diff = {
+        key, type, before, status,
+      };
+      return diff;
+    }
+    const after = obj2[key];
+    const status = 'added';
+    const diff = {
+      key, type, after, status,
+    };
+    return diff;
   });
-  const result = unsortedData;
   return result;
 };
 
-export default(obj1, obj2) => {
+export default (obj1, obj2) => {
   const unsortedDiff = generateDiff(obj1, obj2);
   const resultDiff = sortData(unsortedDiff);
   return resultDiff;
-}
-
-// export default generateDiff;
+};
