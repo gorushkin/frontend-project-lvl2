590fc7705e9e2ab6b8110fb958c411124b27bccc
gorushkin <gorushkin@gmail.com>
2020-05-02 11:20:23 +0300

    убрал лишнии обертки

--- src/formatters/renderFullDiff.js
+++ src/formatters/renderFullDiff.js
@@ -6,12 +6,11 @@ const stringify = (obj, depth) => {
   if (!_.isObject(obj)) {
     return obj;
   }
-  const indent = generateIndent(depth );
+  const indent = generateIndent(depth);
   const notes = Object.keys(obj).map((key) => `${indent}    ${key}: ${obj[key]}`);
   const result = `{\n${notes.join('\n')}\n${indent}}`;
   return result;
 };
-
 
 const notesGenerators = {
   nested: (indent, node, depth, fn) => `${indent}    ${node.key}: ${fn(node.children, depth + 1)}`,
@@ -21,17 +20,12 @@ const notesGenerators = {
   removed: (indent, node, depth) => `${indent}  - ${node.key}: ${stringify(node.before, depth + 1)}`,
 };
 
-const renderFullDiff = (data) => {
-  const iter = (diff, currentDepth = 0) => {
-    const indent = generateIndent(currentDepth);
-    const generatedNote = diff
-    .map((node) => notesGenerators[node.type](indent, node, currentDepth, iter));
-    const result = `{\n${generatedNote.join('\n')}\n${indent}}`
-    return result;
-  };
-
-  const result = iter(data);
-  return result
+const renderFullDiff = (diff, currentDepth = 0) => {
+  const indent = generateIndent(currentDepth);
+  const generatedNote = diff
+    .map((node) => notesGenerators[node.type](indent, node, currentDepth, renderFullDiff));
+  const result = `{\n${generatedNote.join('\n')}\n${indent}}`;
+  return result;
 };
 
 export default renderFullDiff;
--- src/formatters/renderPlainDiff.js
+++ src/formatters/renderPlainDiff.js
@@ -1,31 +1,25 @@
 import _ from 'lodash';
 
 const stringify = (obj) => {
-  const result = (typeof obj === 'object') ? '[complex value]' : obj;
+  const result = (_.isObject(obj)) ? '[complex value]' : obj;
   return result;
 };
 
 const notesGenerators = {
   nested: (node, path, fn) => {
     const newPath = `${path}${node.key}.`;
-    const children = fn(node.children, newPath);
-    return children;
+    return fn(node.children, newPath);
   },
-  unchanged: () => [],
+  unchanged: () => null,
   changed: (node, path) => `Property '${path}${node.key}' was changed from ${stringify(node.before)} to ${stringify(node.after)}`,
   added: (node, path) => `Property '${path}${node.key}' was added with value: ${stringify(node.after)}`,
   removed: (node, path) => `Property '${path}${node.key}' was deleted`,
 };
 
-const renderPlainDiff = (data) => {
-  const iter = (diff, currentPath) => {
-    const result = diff.map((node) => notesGenerators[node.type](node, currentPath, iter));
-    return result;
-  };
-
-  const rawData = iter(data, '');
-  const flattedData = _.flattenDeep(rawData);
-  const result = flattedData.join('\n');
+const renderPlainDiff = (diff, currentPath = '') => {
+  const unFilteredResult = diff
+    .map((node) => notesGenerators[node.type](node, currentPath, renderPlainDiff));
+  const result = unFilteredResult.filter((line) => line).join('\n');
   return result;
 };
 
